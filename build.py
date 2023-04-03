dc7c67a47188eb936b83700e9dc6d389afa6fbe4
DUOLabs333 <dvdugo333@gmail.com>
2023-04-02 20:46:33 -0400

    Move to OOP

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import os, sys,shutil
 import subprocess
+import textwrap
 
 def find_and_replace(filename,old,new):
 	with open(filename,"w") as f:
@@ -30,26 +31,29 @@ for directory in ["build/qemu", "source"]:
 	os.makedirs(os.path.join(root_path,directory),exist_ok=True)
 
 def git_clone(url, branch="",cwd=None):
-	subprocess.run(["git","clone","--depth=1",'--recurse-submodules']+(["--branch" ,branch] if branch else [])+[url],cwd=cwd)
+	subprocess.run(["git","clone","--depth=1",'--shallow-submodules']+(["--branch" ,branch] if branch else [])+[url],cwd=cwd)
 
-if "--depot" in sys.argv:
-	git_clone("https://chromium.googlesource.com/chromium/tools/depot_tools.git")
+class BuildClass:
+	def __init__(self,name):
+		self.name=name
 
-for repo in ["angle","libepoxy","virglrenderer","qemu"]:
-	if (repo in options_dict) and ('download' in options_dict[repo]):
-		git_clone(f"https://github.com/akihikodaki/{repo}.git","macos","source")
+	def run(self):
+		if self.name not in options_dict:
+			return
 
-if '--dependencies' in sys.argv:
-	subprocess.run(["sudo","port","install","meson","ninja"])
-	subprocess.run(["brew","install","glib","pkgconfig"])
+		for method in ['download','configure','build']:
+			if method in options_dict[self.name]:
+				getattr(self,method)()
 
-if "angle" in options_dict:
-	environment["DEPOT_TOOLS_UPDATE"]='0'
-	environment["PATH"]=os.path.join(os.getcwd(),"depot_tools")+os.pathsep+environment["PATH"]
+	def download(self):
+		git_clone(f"https://github.com/akihikodaki/{self.name}.git","macos","source")
 
-	os.chdir("source/angle")
-	if 'download' in options_dict['angle']:
-		import textwrap
+class Angle(BuildClass):
+	def __init__(self):
+		super().__init__("angle")
+
+	def download(self):
+		super().download()
 		subprocess.run(["scripts/bootstrap.py"],env=environment)
 		subprocess.run(["gclient","sync","-D","-j12","--no-history","--shallow"],env=environment)
 		find_and_replace("build/mac/find_sdk.py","best_sdk = sorted(sdks, key=parse_version)[0]","best_sdk = sorted(sdks, key=parse_version)[-1]")
@@ -61,59 +65,96 @@ if "angle" in options_dict:
 				settings['xcode_build']='11.2.0'
 				return
 			"""))
-	if 'configure' in options_dict['angle']:
+	def configure(self):
+		environment["DEPOT_TOOLS_UPDATE"]='0'
+		environment["PATH"]=os.path.join(os.getcwd(),"depot_tools")+os.pathsep+environment["PATH"]
+		os.chdir("source/angle")
 		subprocess.run(["gn","gen", "--args=is_debug=false angle_build_tests=false", "../../build/angle"],env=environment)
-	os.chdir("../..")
-	if 'build' in options_dict['angle']:
+		os.chdir("../..")
+
+	def build(self):
 		subprocess.run(["ninja","-j4","-C","build/angle"])
 
-if "libepoxy" in options_dict:
-	if 'configure' in options_dict['libepoxy']:
-		subprocess.run(["meson", f"-Dc_args=-I{os.getcwd()}/source/angle/include","-Degl=yes", "-Dx11=false", f"--prefix={os.getcwd()}","build/libepoxy","source/libepoxy"])
 
-	if 'build' in options_dict['libepoxy']:
+class Libepoxy(BuildClass):
+	def __init__(self):
+		super().__init__("libepoxy")
+
+	def configure(self):
+		subprocess.run(["meson", f"-Dc_args=-I{os.getcwd()}/source/angle/include","-Degl=yes", "-Dx11=false", f"--prefix={os.getcwd()}","build/libepoxy","source/libepoxy"])
+	def build(self):
 		subprocess.run(["meson","install","-C","build/libepoxy"])
 
-if "virglrenderer" in options_dict:
-	if 'configure' in options_dict['virglrenderer']:
-		subprocess.run(["meson", f"-Dc_args=-I{os.getcwd()}/source/angle/include", f"--pkg-config-path={os.getcwd()}/lib/pkgconfig", f"--prefix={os.getcwd()}","build/virglrenderer","source/virglrenderer"])
-	if 'build' in options_dict['virglrenderer']:
+class Vulkan(BuildClass):
+	def __init__(self):
+		super().__init__("vulkan")
+
+	def download(self):
+		git_clone("https://github.com/KhronosGroup/MoltenVK",cwd="source")
+
+	def configure(self):
+		subprocess.run(["./fetchDependencies","--macos"],cwd="source/MoltenVK")
+
+	def build(self):
+		subprocess.run(["make","macos"],cwd="source/MoltenVK")
+
+
+
+
+
+class Virglrenderer(BuildClass):
+	def __init__(self):
+		super().__init__("virglrenderer")
+
+	def configure(self):
+		subprocess.run(["meson", f"-Dc_args=-I{os.getcwd()}/source/angle/include",         "-Dvenus-experimental=true",
+		f"--pkg-config-path={os.getcwd()}/lib/pkgconfig", f"--prefix={os.getcwd()}","build/virglrenderer","source/virglrenderer"])
+	def build(self):
 		subprocess.run(["meson","install","-C","build/virglrenderer"])
+		
 
-if 'qemu' in options_dict:
-	os.chdir("build/qemu")
-	import textwrap
-	qemu_flags=f"""
-	--disable-bsd-user
-	--disable-guest-agent
-	--disable-gnutls
-	--enable-curses
-	--disable-libssh
-	--disable-nettle
-	--disable-lzo
-	--disable-snappy
-	--disable-zstd
-	--extra-cflags=-DNCURSES_WIDECHAR=1
-	--target-list=aarch64-softmmu
-	--extra-cflags=-I{os.getcwd()}/../../source/angle/include
-	--extra-ldflags=-L{os.getcwd()}/../angle
-	--extra-ldflags=-L/opt/local/lib
-	--extra-cflags=-I/opt/local/include
-	--prefix={os.getcwd()}/../..
-	--disable-sdl
-	--disable-gtk
-	--disable-vde
-	--disable-gio
-	--enable-cocoa
-	--disable-curl"""
-	qemu_flags=textwrap.dedent(qemu_flags).splitlines()
-	environment["PKG_CONFIG_PATH"]=os.getcwd()+"/../../lib/pkgconfig"
-	if 'configure' in options_dict['qemu']:
-		subprocess.run(["../../source/qemu/configure"]+qemu_flags,env=environment)
+class Qemu(BuildClass):
+	def __init__(self):
+		super().__init__("qemu")
+		self.qemu_flags=f"""
+		--disable-bsd-user
+		--disable-guest-agent
+		--disable-gnutls
+		--enable-curses
+		--disable-libssh
+		--disable-nettle
+		--disable-lzo
+		--disable-snappy
+		--disable-zstd
+		--extra-cflags=-DNCURSES_WIDECHAR=1
+		--target-list=aarch64-softmmu
+		--extra-cflags=-I{os.getcwd()}/source/angle/include
+		--extra-ldflags=-L{os.getcwd()}/build/angle
+		--extra-ldflags=-L/opt/local/lib
+		--extra-cflags=-I/opt/local/include
+		--prefix={os.getcwd()}
+		--disable-sdl
+		--disable-gtk
+		--disable-vde
+		--disable-gio
+		--enable-cocoa
+		--disable-curl"""
+		self.qemu_flags=textwrap.dedent(self.qemu_flags).splitlines()
+		environment["PKG_CONFIG_PATH"]=os.getcwd()+"/lib/pkgconfig"
+	def configure(self):
+		subprocess.run(["../../source/qemu/configure"]+qemu_flags,env=environment,cwd="build/qemu")
+	def build(self):
+		subprocess.run(["meson","install"],cwd="build/qemu")
 
-	if 'build' in options_dict['qemu']:
-		subprocess.run(["meson","install"])
-		#subprocess.run(["make","install","-j8"])
+if "--depot" in sys.argv:
+	git_clone("https://chromium.googlesource.com/chromium/tools/depot_tools.git")
+
+if '--dependencies' in sys.argv:
+	subprocess.run(["sudo","port","install","meson","ninja"])
+	subprocess.run(["brew","install","glib","pkgconfig"])
+
+for target in ["angle","libepoxy","vulkan","virglrenderer","qemu"]:
+	eval(f"{target.title()}().run()")
 
 if '--clean' in sys.argv:
 	for directory in ['build','source','depot_tools']:
