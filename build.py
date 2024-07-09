42e7595aa376b10cf63494af6f69b05718adc05f
DUOLabs333 <dvdugo333@gmail.com>
2024-07-09 19:42:45 -0400

    Fix incorrect indentation

--- build.py
+++ build.py
@@ -32,14 +32,14 @@ for directory in ["build/qemu", "source"]:
 
 def delete_everything_in_folder(folder):
 	for filename in os.listdir(folder):
-    file_path = os.path.join(folder, filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
+	    file_path = os.path.join(folder, filename)
+	    try:
+	        if os.path.isfile(file_path) or os.path.islink(file_path):
+	            os.unlink(file_path)
+	        elif os.path.isdir(file_path):
+	            shutil.rmtree(file_path)
+	    except Exception as e:
+	        print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 def git_clone(url, branch="",cwd=None):
 	subprocess.run(["git","clone","--depth=1",'--shallow-submodules']+(["--branch" ,branch] if branch else [])+[url],cwd=cwd)
